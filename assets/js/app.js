121bc8a534ed22295c7bc9f9d962e1404fc26d1a
fullstack360 <info@fullstack360.com>
2017-05-20 12:03:06 -0400

    add places and fetch places

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,7 @@
+var turbo = Turbo({
+	site_id: '591f2766fbd0120011a4d4de'
+})
+
 var place = {
 	name: '',
 	instagram: '',
@@ -6,13 +10,43 @@ var place = {
 	state: '',
 	website: '',
 	icon: '',
-	banner: ''
+	banner: '',
+	location: {
+		lat: 0.0,
+		lng: 0.0
+	}
 }
 
-console.log('PLACE: '+JSON.stringify(place))
+var places = []
+
+var renderPlaces = function(){
+	var listItems = ''
+	places.forEach(function(place, i){
+		listItems += '<li>'+place.name+'</li>'
+	})
+
+	$('#place-list').html(listItems)
+}
+
+
+var fetchPlaces = function(){
+	turbo.fetch('place', null, function(err, data){
+		if (err){
+			alert('ERROR: '+err.message)
+			return
+		}
+
+		if (data.results == null)
+			return
+
+		// console.log('PLACES: '+JSON.stringify(data))
+		places = data.results
+		renderPlaces()
+	})
+}
 
 var updatePlace = function(event){
-	place[event.target.name] = event.target.value
+	place[event.target.name] = event.target.value.trim()
 }
 
 var createPlace = function(event){
@@ -34,11 +68,36 @@ var createPlace = function(event){
 		return
 	}
 
-	console.log('Create Place: '+JSON.stringify(place))
-	var turbo = Turbo({
-		site_id: '591f2766fbd0120011a4d4de'		
-	})
+	if (place.address.length>0 &&  place.city.length>0 && place.state.length>0){
+		// https://maps.googleapis.com/maps/api/geocode/json?address=76+east+7th+street,new+york,NY&key=AIzaSyC44mPpaMNvENXryYjHBHzjST1UMnYlARk
+		var addr = place.address+','+place.city+','+place.state
+		var url = 'https://maps.googleapis.com/maps/api/geocode/json'		
 
+	    $.ajax({
+	        url: url,
+	        type: 'GET',
+	        data: {
+	        	address: addr,
+	        	key: 'AIzaSyC44mPpaMNvENXryYjHBHzjST1UMnYlARk'
+	        },
+	        contentType: 'application/json; charset=utf-8',
+	        dataType: 'json',
+	        async: true,
+	        success: function(data, status) {
+				console.log('GEOCODE: '+JSON.stringify(data))
+		    	// completion(null, data)
+	        },
+		    error: function(xhr, status, error) { 
+	        	// console.log('FAIL: '+JSON.stringify(error))
+		    	// completion(error, null)
+		    }
+	    })
+
+
+		return
+	}
+
+	console.log('Create Place: '+JSON.stringify(place))
 	turbo.create('place', place, function(err, data){
 		if (err){
 			alert('ERROR: '+err.message)
@@ -46,6 +105,9 @@ var createPlace = function(event){
 		}
 
 		console.log('Place Created: '+JSON.stringify(data))
+		var newPlace = data.result
+		places.unshift(newPlace)
+		renderPlaces()
 	})
 
 }
