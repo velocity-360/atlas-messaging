71528406d806f23f6dfef591d64329f345d27741
fullstack360 <info@fullstack360.com>
2017-05-24 10:19:54 -0400

    http post request

--- src/utils/HTTPClient.js
+++ src/utils/HTTPClient.js
@@ -5,31 +5,35 @@ module.exports = {
 
 	getRequest: (endpoint, params) => {
 		return new Promise((resolve, reject) => {
-			// if (queue.indexOf(endpoint) != -1) // request already running, ignore
-			// 	return
-			// queue.push(endpoint)
-			
 			superagent
 			.get(endpoint)
 			.query(params)
 			.set('Accept', 'application/json')
 			.end((err, res) => {
-				// let index = queue.indexOf(endpoint)
-				// queue.splice(index, 1)
-
 				if (err){ 
 					reject(err)
 					return
 				}
-
-				// if (res.body.confirmation != 'success'){
-				// 	reject({message:res.body.message})
-				// 	return
-				// }
 
 				resolve(res.body)
 			})
 		})
 	},
 
+	postRequest: (endpoint, params) => {
+		return new Promise((resolve, reject) => {
+			superagent
+			.post(endpoint)
+			.send(params)
+			.set('Accept', 'application/json')
+			.end((err, res) => {
+				if (err){ 
+					reject(err)
+					return
+				}
+
+				resolve(res.body)
+			})
+		})
+	}
 }
