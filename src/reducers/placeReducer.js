ef5d1b450991555e39aaa6d50cbc30581a713a31
fullstack360 <info@fullstack360.com>
2017-05-25 12:14:06 -0400

    comment

--- src/reducers/placeReducer.js
+++ src/reducers/placeReducer.js
@@ -11,8 +11,6 @@ export default (state = initialState, action) => {
 
 		case constants.PLACES_RECEIVED:
 			// console.log('PLACES_RECEIVED: '+JSON.stringify(action.data))
-			
-			// const venues = action.data.response.venues // this is unique to Foursquare API. Change when switching.
 			action.data.forEach((venue, i) => {
 				newState[venue.id] = venue
 			})
