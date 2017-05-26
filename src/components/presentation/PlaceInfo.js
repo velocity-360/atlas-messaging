de2d241765dc8594790ced374e3af45203081e16
fullstack360 <info@fullstack360.com>
2017-05-26 13:30:25 -0400

    show place info at top of feed

--- src/components/presentation/PlaceInfo.js
+++ src/components/presentation/PlaceInfo.js
@@ -1,39 +1,53 @@
 import React, { Component } from 'react'
 
 export default (props) => {
-
 	const place = props // can be null
 
 	let name = ''
 	let image = ''
 	let website = ''
 	let description = ''
+	let address = ''
 
-	if (place){
+	if (place){ // TODO: populate other attributes
 		name = place.name
-		// TODO: populate other attributes
+		image = place.icon
+		address = place.address
+		website = place.website || ''
+		if (website.indexOf('http')==-1 && website.length>0)
+			website = 'http://'+website
 	}
-
 
 	return (
 		<div style={localStyle.container}>
-			<h3 className="notopmargin" style={localStyle.header}>
-				{name}
-			</h3>
+			<img src={image} style={localStyle.image} />
+			<div style={{padding:16}}>
+				<h3 className="notopmargin" style={localStyle.header}>
+					{name}
+				</h3>
+				<hr />
 
+				<span style={{fontWeight:100}}>{address}</span><br />
+				{ (website.length > 0) ? <a target="_blank" href={website}>Website</a> : null }
+			</div>
 		</div>
 	)
 }
 
 const localStyle = {
 	container: {
+		border: '1px solid #ddd',
 		background: '#fff',
-		marginBottom: 24,
-		padding: 16
+		marginBottom: 24
+	},
+	image: {
+		width: 100,
+		float: 'right',
+		marginLeft: 16
 	},
 	header: {
 		fontFamily: 'Pathway Gothic One',
-		fontWeight: 100
+		fontWeight: 100,
+		marginBottom: 0
 	}
-
 }
